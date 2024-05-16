10f655879120a108561b91e6746fad5226ca9059
yehani <yehanipamunuwa@gmail.com>
2024-05-16 17:58:12 +0530

    add background image for cal

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+/*
+const themeToggleButton = document.querySelector(".theme-changer-toggle");
+const calculator = document.querySelector(".calculator");
+const toggleIcon = document.querySelector(".toggle-icon");
+let isDark = true;
+themeToggleButton.onclick = () => {
+    calculator.classList.toggle("dark");
+    themeToggleButton.classList.toggle("active");
+    isDark = !isDark;
+};*/
